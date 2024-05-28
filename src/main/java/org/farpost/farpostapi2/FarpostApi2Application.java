8f524e477f0e583df128ae5e168a48e438ed554a
JlWorker <567842bm@bk.ru>
2024-05-28 17:26:12 +1000

    Start project

---
+++ src/main/java/org/farpost/farpostapi2/FarpostApi2Application.java
@@ -0,0 +1,13 @@
+package org.farpost.farpostapi2;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication()
+public class FarpostApi2Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(FarpostApi2Application.class, args);
+    }
+
+}
